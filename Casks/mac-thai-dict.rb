44b87f2caa74f8c17c4735f492c57a69fb3ed48d
“teerasak” <teerasak@thoughtworks.com>
2021-04-23 00:28:22 +0700

    change to LEXiTRON dict

--- Casks/mac-thai-dict.rb
+++ Casks/mac-thai-dict.rb
@@ -1,11 +1,11 @@
 cask 'mac-thai-dict' do
-  version '1.0.1'
-  sha256 'c4832b5d57106b47d3af36b2c562ced348fd25462727a92730291aa828f0a926'
+  version '0.02'
+  sha256 '00971cde865fc1df88a3447a16ec7cf806ab439c42bf146552a9289bb983fa45'
 
-  url "https://storage.googleapis.com/codeforthailand/mac-thai-dict-#{version}.pkg"
-  homepage "https://www.macthai.com/2015/11/05/install-dictionary-th-en-on-mac-osx/"
+  url "https://github.com/garfieldnate/Lexitron-Mac/releases/download/v0.02/LEXiTRON.English-Thai.Thai-English-v#{version}.dictionary.zip"
+  name "LEXiTRON Mac Dictionary"
+  desc "LEXiTRON English-Thai & Thai-English dictionary"
+  homepage "https://github.com/garfieldnate/Lexitron-Mac"
 
-  pkg "mac-thai-dict-#{version}.pkg"
-
-  uninstall pkgutil: 'com.infinisoft.thaiDictionaries.thaiDictionaries.pkg'
+  dictionary "LEXiTRON English-Thai, Thai-English.dictionary"
 end
